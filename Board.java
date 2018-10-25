808d0e7df436b1066cbb6b3f1fd11220a1f8314c
MysticMan1 <44099131+MysticMan1@users.noreply.github.com>
2018-10-24 23:31:15 -0700

    Add files via upload
    Updated, fixed version.

--- Board.java
+++ Board.java
@@ -1,13 +1,10 @@
 
 public class Board {
     int displayScore;//recording of game's current score
-    String[] arrayOfRoutes= new String[50];//routes between destinations, used for points
-    String[] destinationPlaces= new String[25];//destination names
-    arrayOfRoutes[0]="Raleigh->Chicago";//hardcoded preset example 
-    destinationPlaces[0]="Raleigh";//hardcode
-    destinationPlaces[1]="Chicago";//hardcode
-    int[] scoreMarkerArray={20,13,4,15};
-    int displayScore()
+    String[] arrayOfRoutes= {"Raleigh->Chicago"};//example routes between destinations, used for points
+    String[] destinationPlaces= {"Raleigh","Chicago"};//example destination names
+    int[] scoreMarkerArray={20,13,4,15};//temporary scores for four players
+    public int displayScore()
     {
     	return displayScore;
     }
@@ -15,7 +12,7 @@ public class Board {
     {
     	return"Train Type: Reefer";//Just as an example
     }
-    public int scoreMarker()
+    public void scoreMarker()
     {
     	for (int i=0;i<4;i++)
     	{
