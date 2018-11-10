55e8e9e2c82ec13d6996eb14f0de34e9ca036fa3
MysticMan1 <44099131+MysticMan1@users.noreply.github.com>
2018-11-09 19:59:45 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package route;
+
+/**
+ *
+ * @author Ahssan
+ */
+//wherein the actual Main program is played, and all objects are initialized
+public class Main {
+    public static void main(String args[])
+    {
+     //Ahssan: initializing all routes
+    Route route1Version1 = new Route("Los Angeles-to-San Francisco", 3, "Pink");
+    Route route1Version2 = new Route("Los Angeles-to-San Francisco", 3, "Yellow");
+    Route route2Version1 = new Route("San Francisco-to-Portland", 5, "Pink");
+    Route route2Version2 = new Route("San Francisco-to-Portland", 5, "Pink");
+    Route route3Version1 = new Route("Portland-to-Seattle", 1, "Gray");
+    Route route3Version2 = new Route("Portland-to-Seattle", 1, "Gray");
+    //initializng board
+    /*----initialize board here by replacing this line*/
+    //initialize Deck, cards
+    /*initialize dec, cards here by replacing this line*/
+    }
+}
